1281bd787c3a13a72de00746edff537b7be6b6bb
stern <ds@zettelstore.de>
2023-10-15 17:58:16 +0000

    Initial draft of new Builtin objects
    FossilOrigin-Name: d8a09ddeebc7c729942283e84a5f4fba1945ed581a2037bbffe573df0195ebdd

--- sxeval/builtin.go
+++ sxeval/builtin.go
@@ -11,11 +11,98 @@
 package sxeval
 
 import (
+	"fmt"
 	"io"
 	"reflect"
 
 	"zettelstore.de/sx.fossil"
 )
+
+// Builtin is the type for normal builtin functions.
+type Builtin struct {
+	// The canonical name of the builtin
+	name string
+
+	// The actual builtin function
+	proc func(*Frame, []sx.Object) (sx.Object, error)
+
+	// Minimum and maximum arity. If maxArity < minArity, maximum arity is unlimited
+	minArity, maxArity int
+
+	// Will a call to the builtin produce some side effect?
+	hasSideEffect bool
+}
+
+// HasSideEffect retruns true, if a call to the builtin may change the environment.
+func (b *Builtin) HasSideEffect() bool { return b != nil && b.hasSideEffect }
+
+// --- Builtin methods to implement sx.Object
+
+// IsNil checks if the concrete object is nil.
+func (b *Builtin) IsNil() bool { return b == nil }
+
+// IsAtom returns true iff the object is an object that is not further decomposable.
+func (b *Builtin) IsAtom() bool { return b == nil }
+
+// IsEqual compare two objects for deep equality.
+func (b *Builtin) IsEqual(other sx.Object) bool {
+	if b == other {
+		return true
+	}
+	if b == nil {
+		return sx.IsNil(other)
+	}
+	if otherB, ok := other.(*Builtin); ok {
+		return b.name == otherB.name
+	}
+	return false
+
+}
+
+// Repr returns the object representation.
+func (b *Builtin) Repr() string { return sx.Repr(b) }
+
+// String returns go representation.
+func (b *Builtin) String() string { return b.Repr() }
+
+func (b *Builtin) Print(w io.Writer) (int, error) {
+	return sx.WriteStrings(w, "#<builtin:", b.name, ">")
+}
+
+// --- Builtin methods to implement sxeval.Callable
+
+// Call the builtin function with the given frame and arguments.
+func (b *Builtin) Call(frame *Frame, args []sx.Object) (sx.Object, error) {
+	// Check arity
+	numArgs, minArity, maxArity := len(args), b.minArity, b.maxArity
+	if minArity == maxArity {
+		if numArgs != minArity {
+			return nil, fmt.Errorf("exactly %d arguments required, but %d given: %v", minArity, numArgs, args)
+		}
+	} else if minArity > maxArity {
+		if numArgs < minArity {
+			return nil, fmt.Errorf("at least %d arguments required, but only %d given: %v", minArity, numArgs, args)
+		}
+	} else {
+		if numArgs < minArity || maxArity < numArgs {
+			return nil, fmt.Errorf("between %d and %d arguments required, but %d given: %v", minArity, maxArity, numArgs, args)
+		}
+	}
+
+	obj, err := b.proc(frame, args)
+	if err == nil {
+		return obj, nil
+	}
+	if _, ok := (err).(executeAgain); ok {
+		return obj, err
+	}
+	if _, ok := err.(CallError); !ok {
+		err = CallError{Name: b.name, Err: err}
+	}
+	return obj, err
+}
+
+// --- The following code is deprecated.
 
 // BuiltinOld is a callable with a name
 type BuiltinOld interface {
