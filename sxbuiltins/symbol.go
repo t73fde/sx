31c20bec9457da99adbe0e28335fc67003273327
stern <ds@zettelstore.de>
2025-04-04 11:03:11 +0000

    Add builtin function (symbol? x)
    FossilOrigin-Name: edcfd40b6ac0a1dc52a393edc97c4df5d59a97a9e802e46fbd07f2193eca7611

---
+++ sxbuiltins/symbol.go
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2025-present Detlef Stern
+//
+// This file is part of sx.
+//
+// sx is licensed under the latest version of the EUPL (European Union
+// Public License). Please see file LICENSE.txt for your rights and obligations
+// under this license.
+//
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2025-present Detlef Stern
+//-----------------------------------------------------------------------------
+
+package sxbuiltins
+
+import (
+	"t73f.de/r/sx"
+	"t73f.de/r/sx/sxeval"
+)
+
+// SymbolP returns true if the argument is a symbol.
+var SymbolP = sxeval.Builtin{
+	Name:     "symbol?",
+	MinArity: 1,
+	MaxArity: 1,
+	TestPure: sxeval.AssertPure,
+	Fn1: func(_ *sxeval.Environment, arg sx.Object) (sx.Object, error) {
+		_, isSymbol := sx.GetSymbol(arg)
+		return sx.MakeBoolean(isSymbol), nil
+	},
+}
