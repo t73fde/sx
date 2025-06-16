28f506bd8c418a62e2fd89dd2d6c2073a3723662
stern <ds@zettelstore.de>
2025-06-16 15:59:39 +0000

    Make defference between frames and global bindings/environment more explicit
    FossilOrigin-Name: 7f613fd4023d88eb11e54d83981ed9e6eb34cf72b2caf317a9a5e3f12dc92d60

--- sxbuiltins/binding.go
+++ sxbuiltins/binding.go
@@ -13,58 +13,16 @@
 
 package sxbuiltins
 
-// Provides some special/builtin functions to work with environments.
+// Provides some special/builtin functions to work with bindings.
 
 import (
 	"t73f.de/r/sx"
 	"t73f.de/r/sx/sxeval"
 )
 
-// CurrentBinding returns the current frame.
-var CurrentBinding = sxeval.Builtin{
-	Name:     "current-binding",
-	MinArity: 0,
-	MaxArity: 0,
-	TestPure: nil,
-	Fn0: func(_ *sxeval.Environment, frame *sxeval.Frame) (sx.Object, error) {
-		return frame, nil
-	},
-}
-
-// ParentBinding returns the parent frame of the given frame. For the
-// top-most frame, NIL is returned.
-var ParentBinding = sxeval.Builtin{
-	Name:     "parent-binding",
-	MinArity: 1,
-	MaxArity: 1,
-	TestPure: sxeval.AssertPure,
-	Fn1: func(_ *sxeval.Environment, arg sx.Object, _ *sxeval.Frame) (sx.Object, error) {
-		frame, err := GetFrame(arg, 0)
-		if err != nil {
-			return nil, err
-		}
-		return frame.Parent(), nil
-	},
-}
-
-// Bindings returns the given frame as an association list.
-var Bindings = sxeval.Builtin{
-	Name:     "bindings",
-	MinArity: 1,
-	MaxArity: 1,
-	TestPure: sxeval.AssertPure,
-	Fn1: func(_ *sxeval.Environment, arg sx.Object, _ *sxeval.Frame) (sx.Object, error) {
-		frame, err := GetFrame(arg, 0)
-		if err != nil {
-			return nil, err
-		}
-		return frame.Bindings(), nil
-	},
-}
-
-// BoundP returns true, if the given symbol is bound in the current environment.
-var BoundP = sxeval.Builtin{
-	Name:     "bound?",
+// SymbolBoundP returns true, if the given symbol is bound in the current environment.
+var SymbolBoundP = sxeval.Builtin{
+	Name:     "symbol-bound?",
 	MinArity: 1,
 	MaxArity: 1,
 	TestPure: nil,
@@ -78,45 +36,10 @@ var BoundP = sxeval.Builtin{
 	},
 }
 
-// BindingLookup returns the symbol's bound value in the given
-// frame, or Undefined if there is no such value.
-var BindingLookup = sxeval.Builtin{
-	Name:     "binding-lookup",
-	MinArity: 1,
-	MaxArity: 2,
-	TestPure: nil,
-	Fn1: func(_ *sxeval.Environment, arg sx.Object, frame *sxeval.Frame) (sx.Object, error) {
-		sym, err := GetSymbol(arg, 0)
-		if err != nil {
-			return nil, err
-		}
-		if obj, found := frame.Lookup(sym); found {
-			return obj, nil
-		}
-		return sx.MakeUndefined(), nil
-	},
-	Fn: func(_ *sxeval.Environment, args sx.Vector, _ *sxeval.Frame) (sx.Object, error) {
-		sym, err := GetSymbol(args[0], 0)
-		if err != nil {
-			return nil, err
-		}
-		if arg1 := args[1]; !sx.IsNil(arg1) {
-			frame, err2 := GetFrame(arg1, 1)
-			if err2 != nil {
-				return nil, err2
-			}
-			if obj, found := frame.Lookup(sym); found {
-				return obj, nil
-			}
-		}
-		return sx.MakeUndefined(), nil
-	},
-}
-
-// BindingResolve returns the symbol's bound value in the given frame
+// ResolveSymbol returns the symbol's bound value in the given frame
 // and all its parent frame and in the global binding, or Undefined if there is no such value.
-var BindingResolve = sxeval.Builtin{
-	Name:     "binding-resolve",
+var ResolveSymbol = sxeval.Builtin{
+	Name:     "resolve-symbol",
 	MinArity: 1,
 	MaxArity: 2,
 	TestPure: nil,
@@ -135,9 +58,13 @@ var BindingResolve = sxeval.Builtin{
 		if err != nil {
 			return nil, err
 		}
+
 		frame, err := GetFrame(args[1], 1)
 		if err != nil {
-			return nil, err
+			if !sx.IsNil(args[1]) {
+				return nil, err
+			}
+			frame = nil
 		}
 		if obj, found := env.Resolve(sym, frame); found {
 			return obj, nil
---
+++ sxbuiltins/frame_test.go
@@ -0,0 +1,89 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2025-present Detlef Stern
+//
+// This file is part of sx.
+//
+// sx is licensed under the latest version of the EUPL (European Union
+// Public License). Please see file LICENSE.txt for your rights and obligations
+// under this license.
+//
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2025-present Detlef Stern
+//-----------------------------------------------------------------------------
+
+package sxbuiltins_test
+
+import "testing"
+
+func TestFrame(t *testing.T) {
+	t.Parallel()
+	tcsFrame.Run(t)
+}
+
+var tcsFrame = tTestCases{
+	{name: "err-current-frame-1",
+		src:     "(current-frame 1)",
+		exp:     "{[{current-frame: exactly 0 arguments required, but 1 given: [1]}]}",
+		withErr: true,
+	},
+
+	{name: "err-parent-frame-0",
+		src:     "(parent-frame)",
+		exp:     "{[{parent-frame: exactly 1 arguments required, but none given}]}",
+		withErr: true,
+	},
+	{name: "err-parent-frame-1-noenv",
+		src:     "(parent-frame 1)",
+		exp:     "{[{parent-frame: argument 1 is not a frame, but sx.Int64/1}]}",
+		withErr: true,
+	},
+	{name: "parent-frame-current", src: "(parent-frame (current-frame))", exp: "()"},
+	// {name: "parent-frame-1-root", src: "(parent-frame ROOT)", exp: "#<undefined>"},
+
+	{name: "err-bindings-0",
+		src:     "(bindings)",
+		exp:     "{[{bindings: exactly 1 arguments required, but none given}]}",
+		withErr: true,
+	},
+	{name: "err-bindings-1-noenv",
+		src:     "(bindings 1)",
+		exp:     "{[{bindings: argument 1 is not a frame, but sx.Int64/1}]}",
+		withErr: true,
+	},
+	{name: "current-bindings", src: "(bindings (current-frame))", exp: "()"},
+	{name: "let-bindings",
+		src: "(let ((a 3)) (bindings (current-frame)))",
+		exp: "((a . 3))",
+	},
+
+	{name: "err-lookup-0",
+		src:     "(frame-lookup)",
+		exp:     "{[{frame-lookup: between 1 and 2 arguments required, but none given}]}",
+		withErr: true,
+	},
+	{name: "err-lookup-1-nosym",
+		src:     "(frame-lookup 1)",
+		exp:     "{[{frame-lookup: argument 1 is not a symbol, but sx.Int64/1}]}",
+		withErr: true,
+	},
+	{name: "err-lookup-1-nosym-2",
+		src:     "(frame-lookup 1 2)",
+		exp:     "{[{frame-lookup: argument 1 is not a symbol, but sx.Int64/1}]}",
+		withErr: true,
+	},
+	{name: "err-lookup-2-noenv",
+		src:     "(frame-lookup 'a 1)",
+		exp:     "{[{frame-lookup: argument 2 is not a frame, but sx.Int64/1}]}",
+		withErr: true,
+	},
+	{name: "err-lookup-3-noenv",
+		src:     "(frame-lookup 'a (current-frame) 1)",
+		exp:     "{[{frame-lookup: between 1 and 2 arguments required, but 3 given: [a (#<builtin:current-frame>) 1]}]}",
+		withErr: true,
+	},
+	// {name: "lookup-a", src: "(defvar a 3)(binding-lookup 'a)", exp: "3 3"},
+	{name: "lookup-a", src: "(let ((a 3)) (frame-lookup 'a))", exp: "3"},
+	{name: "lookup-a-2", src: "(let ((a 3)) (frame-lookup 'a (current-frame)))", exp: "3"},
+	{name: "lookup-b", src: "(frame-lookup 'b)", exp: "#<undefined>"},
+	{name: "lookup-b-2", src: "(frame-lookup 'b (current-frame))", exp: "#<undefined>"},
+}
